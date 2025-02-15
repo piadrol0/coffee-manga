ff9a57a029d80ad272e9a141f6bb41ee6f28ed7a
arya <piadrol0@gmail.com>
2025-02-15 00:44:33 -0800

    demo4

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ menu.addEventListener("click", () => {
     menu.setAttribute("class", "fa-solid fa-bars");
   }
 });
+window.addEventListener('scroll', function() {
+  const animatedTexts = document.querySelectorAll('.animated-text'); // انتخاب تمام عناصر
+  const screenPosition = window.innerHeight / 1.2; // 1.2 برای اینکه زودتر ظاهر شود
+
+  animatedTexts.forEach(animatedText => { // حلقه برای هر عنصر
+      const textPosition = animatedText.getBoundingClientRect().top;
+
+      if (textPosition < screenPosition) {
+          animatedText.classList.add('visible'); // اضافه کردن کلاس visible
+          animatedText.style.animation = 'slideIn 600ms ease-in-out'; // اجرای انیمیشن
+      }
+  });
+});
+window.addEventListener('scroll', function() {
+  const animatedTextl = document.querySelectorAll('.animated-textl'); // انتخاب تمام عناصر
+  const screenPosition = window.innerHeight / 1; // 1.2 برای اینکه زودتر ظاهر شود
+
+  animatedTextl.forEach(animatedText => { // حلقه برای هر عنصر
+      const textPosition = animatedText.getBoundingClientRect().top;
+
+      if (textPosition < screenPosition) {
+          animatedText.classList.add('visible'); // اضافه کردن کلاس visible
+          animatedText.style.animation = 'all 600ms linear'; // اجرای انیمیشن
+      }
+  });
+});
+
