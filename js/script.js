c2938b4a562754ca4ed0a83824d2be9a46e5d7f6
arya <piadrol0@gmail.com>
2025-05-15 03:33:02 -0700

    ui changed-2

--- js/script.js
+++ js/script.js
@@ -5,36 +5,40 @@ menu.addEventListener("click", () => {
   if (subMenu.style.display === "none" || subMenu.style.display === "") {
     subMenu.style.display = "block";
     menu.setAttribute("class", "fa-solid fa-xmark");
-
   } else {
     subMenu.style.display = "none";
     menu.setAttribute("class", "fa-solid fa-bars");
   }
 });
-window.addEventListener('scroll', function() {
-  const animatedTexts = document.querySelectorAll('.animated-text');
-  const screenPosition = window.innerHeight / 1.2; 
+window.addEventListener("scroll", function () {
+  const animatedTexts = document.querySelectorAll(".animated-text");
+  const screenPosition = window.innerHeight / 1.2;
 
-  animatedTexts.forEach(animatedText => { 
-      const textPosition = animatedText.getBoundingClientRect().top;
+  animatedTexts.forEach((animatedText) => {
+    const textPosition = animatedText.getBoundingClientRect().top;
 
-      if (textPosition < screenPosition) {
-          animatedText.classList.add('visible'); 
-          animatedText.style.animation = 'slideIn 600ms ease-in-out'; 
-      }
+    if (textPosition < screenPosition) {
+      animatedText.classList.add("visible");
+      animatedText.style.animation = "slideIn 600ms ease-in";
+    }
   });
 });
-window.addEventListener('scroll', function() {
-  const animatedTextl = document.querySelectorAll('.animated-textl'); 
+window.addEventListener("scroll", function () {
+  const animatedTextl = document.querySelectorAll(".animated-textl");
   const screenPosition = window.innerHeight / 1;
 
-  animatedTextl.forEach(animatedText => { 
-      const textPosition = animatedText.getBoundingClientRect().top;
+  animatedTextl.forEach((animatedText) => {
+    const textPosition = animatedText.getBoundingClientRect().top;
 
-      if (textPosition < screenPosition) {
-          animatedText.classList.add('visible');
-          animatedText.style.animation = 'all 600ms linear';
-      }
+    if (textPosition < screenPosition) {
+      animatedText.classList.add("visible");
+      animatedText.style.animation = "all 600ms linear";
+    }
   });
 });
-
+let price = document.getElementById("price");
+let overlay = document.getElementById("overlay");
+setTimeout(() => {
+    price.style.display = "none";
+    overlay.style.display = "none"; 
+}, 3000);
